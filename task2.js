93129c8aa4ed97ee72125444c620072e224e5b4f
Yevhen Mukhamedov <Yevhen_Mukhamedov@epam.com>
2021-03-19 15:15:53 +0200

    fixed loading file not to RAM

--- task2.js
+++ task2.js
@@ -1,24 +1,27 @@
 const csvFilePath='./csv/nodejs-hw1-ex1.csv';
 const csv=require('csvtojson');
+const { Transform  } = require('stream');
 const fs = require('fs');
 
-csv()
-.fromFile(csvFilePath)
-.then((jsonObj)=>{
-    const jsonData = jsonObj.map(json => {
-        let data = {}
-        delete json['Amount'];
-        for(let key in json){
-            data[key.toLowerCase()] = json[key];
-        }
-        return JSON.stringify(data);
-    }).join('\n');
-    fs.writeFile("test.txt", jsonData, function(err) {
-        if (err) {
-            console.log(err);
-        }
-    });
-})
-.catch(error => {
-    console.log(error);
+const transformData = (chunk) => {
+    let data = {};
+    let json = JSON.parse(chunk);
+    delete json['Amount'];
+    for(let key in json){
+        data[key.toLowerCase()] = json[key];
+    }
+    return JSON.stringify(data) + "\n";
+}
+
+const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+        this.push(transformData(chunk.toString()));
+        callback();
+    }
 });
+
+const readStream=fs.createReadStream(csvFilePath);
+
+const writeStream=fs.createWriteStream("test.txt");
+
+readStream.pipe(csv()).pipe(transformStream).pipe(writeStream);
